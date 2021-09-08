58453693c25395fdba9c8128fc636d3a28eba9ea
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-09-08 05:09:41 +0000

    added even numbers and loopy lighthouse

---
+++ even.js
@@ -0,0 +1,26 @@
+/*const isEven = function (num)
+{
+  var even = num % 2 === 0;
+  return even;
+}
+console.log(isEven(5));
+*/
+/*
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(105));
+console.log(isEven(152));
