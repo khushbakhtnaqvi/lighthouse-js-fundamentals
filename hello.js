2046aa056df14549f04610d00522df57b1fa1716
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-09-08 04:57:19 +0000

    added function to say hello

---
+++ hello.js
@@ -0,0 +1,14 @@
+/*const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+*/
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Julie");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
